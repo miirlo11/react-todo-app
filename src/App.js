e06afc3881e4df4de8793824df4d99f4ba692d49
miirlo11 <miirlo11@gmail.com>
2023-01-31 21:10:34 -0600

    React Todo App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import logo from './logo.svg';
 import TodoItem from './Components/TodoItem';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+      {id:1, text:'Sacar la basura.'},
+      {id:2, text:'Pasear al perro.'},
+      {id:3, text:'Cerrar las puertas.'},
+    ],
+    newTodo: '',
+  };
+
+}
+  render(){
   return (
     <div className="App">
       <header className="App-header">
+        <h1>TODO</h1>
+        <ul>
+           {this.state.todos.map((todo) => {
+            return(<TodoItem key={todo.id} text={todo.text}/>
+            );
+          })}
+           {/*this.todos.map(function(todo){
+            return <TodoItem text={todo.text}/>; 
+           })*/}
+        </ul>
         <h1> Hola React Taller-02</h1>
-        <TodoItem/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="" onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newTodo} onChange={this.handleChange}/>
+          <button type='submit'>ADD TODO</button>
+        </form>
       </header>
     </div>
   );
+  }
+
+  handleChange = (event)=>{
+    this.setState({ newTodo: event.target.value });
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState( prevState => ({
+      todos: [],
+      newTodo: '',
+    }));
+  }
 }
 
 export default App;
--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 class TodoItem extends React.Component{
     render(){
-        return <h1> Esto es React! Semana-02</h1>
+        return (
+            <li>
+                {this.props.text}
+            </li>
+        );
     }
 }
 
