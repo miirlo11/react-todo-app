69ce72a73020eea91a215542da49d42ab45354ef
miirlo11 <miirlo11@gmail.com>
2023-01-31 20:12:45 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import TodoItem from './Components/TodoItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Hola React Taller-02</h1>
+        <TodoItem/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class TodoItem extends React.Component{
+    render(){
+        return <h1> Esto es React! Semana-02</h1>
+    }
+}
+
+export default TodoItem;
