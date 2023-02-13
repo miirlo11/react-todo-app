94974af01bf4ea44e932353201f9a094586de010
miirlo11 <miirlo11@gmail.com>
2023-02-12 18:09:07 -0600

    checkbox

--- src/App.js
+++ src/App.js
@@ -8,27 +8,28 @@ class App extends React.Component {
     super();
     this.state = {
       todos: [
-      {id:1, text:'Sacar la basura.'},
-      {id:2, text:'Pasear al perro.'},
-      {id:3, text:'Cerrar las puertas.'},
+      {id:1, text:'Sacar la basura'},
+      {id:2, text:'Pasear al perro'},
+      {id:3, text:'Cerrar las puertas'},
     ],
     newTodo: '',
   };
 
 }
-  render(){
+render(){
   return (
     <div className="App">
       <header className="App-header">
         <h1>TODO</h1> 
         <ul>
            {this.state.todos.map((todo) => {
-            return(<TodoItem key={todo.id} text={todo.text}/>
+            return (
+              <li key={todo.id} class="lil">
+                <TodoItem text={todo.text}/>
+                <input class="checkbox" type="checkbox"/>
+              </li>
             );
           })}
-           {/*this.todos.map(function(todo){
-            return <TodoItem text={todo.text}/>; 
-           })*/}
         </ul>
         <h1> Hola React Taller-02</h1>
         <img src={logo} className="App-logo" alt="logo" />
@@ -39,7 +40,7 @@ class App extends React.Component {
       </header>
     </div>
   );
-  }
+}
 
   handleChange = (event)=>{
     this.setState({ newTodo: event.target.value });
